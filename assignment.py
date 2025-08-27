8050376f1dc1b7a68891b9dc15526473b91764e6
WooChangKim <nanenchanga2@gmail.com>
2025-08-27 10:46:08 +0900

    first

--- assignment.py
+++ assignment.py
@@ -1,9 +1,10 @@
 name = "Alice"
 age = 20
 numbers = [1, 2, 3]
-is_student = False
+is_student = True
 
 print(f"이름: {name}")
 print(f"나이: {age}")
 print(f"숫자 리스트: {numbers}")
 print(f"학생 여부: {is_student}")
+#adlfjlkja
